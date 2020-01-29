bae48ec878f8ef666615694ef7731245b1020747
rmoetwil <ronald.moetwil@gmail.com>
2020-01-29 19:57:01 +0100

    Terraform first steps vnet for vm.

--- terraform/azure-vms.tf
+++ terraform/azure-vms.tf
@@ -1,11 +1,55 @@
-# Configure the Azure Provider
 provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=1.38.0"
+  version = "=1.42.0"
 }
+
+locals {
+  project_name        = "exam-prep"
+  location            = "West Europe"
+  resource_group_name = format("%s-rg", local.project_name)
+  vnet_name           = format("%s-vnet", local.project_name)
+  nsg_name            = format("%s-nsg", local.project_name)
+}
+
 
 # Create a resource group
 resource "azurerm_resource_group" "exam-prep-rg" {
-  name     = "exam-prep-rg"
-  location = "West Europe"
+  name     = local.resource_group_name
+  location = local.location
 }
+
+resource "azurerm_virtual_network" "aexam-prep-vnet" {
+  name                = local.vnet_name
+  resource_group_name = azurerm_resource_group.exam-prep-rg.name
+  location            = azurerm_resource_group.exam-prep-rg.location
+  address_space       = ["10.0.0.0/24"]
+
+  subnet {
+    name           = "default"
+    address_prefix = "10.0.0.0/24"
+    security_group = azurerm_network_security_group.exam-prep-nsg.id
+  }
+}
+
+resource "azurerm_network_security_group" "exam-prep-nsg" {
+  name                = local.nsg_name
+  resource_group_name = azurerm_resource_group.exam-prep-rg.name
+  location            = azurerm_resource_group.exam-prep-rg.location
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# TODO 
+# nic
+# vm
+# public ip
+# scheduler
